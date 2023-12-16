3e1f382eb10266bf78ef8377500b79f8b27bc623
Giulio <giulio.pizzinato@gmail.com>
2023-12-16 10:56:50 +0100

    Modifica path produzione

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 // const path = require('path');
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'? '/ordini_web/':'/',
+  publicPath: process.env.NODE_ENV === 'production'? '/':'/',
 });
 
 // module.exports = {
