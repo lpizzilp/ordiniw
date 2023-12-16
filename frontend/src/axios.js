435e477ce274e342046565f4476e9e25b176ff09
lpizzi <lpizzi00@gmail.com>
2023-12-16 12:02:47 +0100

    Modifica axios per la produzione

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -3,5 +3,8 @@ import axios from "axios";
 window.axios = axios
 axios.defaults.withCredentials = false
 // axios.defaults.baseURL = "http://localhost:8081/api"
-let backendUrl = "http://" + window.location.hostname.toString() + ":8081/api"
+let URLProd = "http://" + window.location.hostname.toString() + "/api"
+let URLDev ="http://" + window.location.hostname.toString() + ":8081/api"
+
+let backendUrl = process.env.NODE_ENV === 'production' ?  URLProd : URLDev 
 axios.defaults.baseURL = backendUrl
