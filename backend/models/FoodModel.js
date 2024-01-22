fb6aa951a09e3b313aaf9dc41830063f27d46a57
DESKTOP-ISD7DOL\Giuli <giulio.pizzinato@gmail.com>
2024-01-22 17:15:37 +0100

    Articoli con validità

--- backend/models/FoodModel.js
+++ backend/models/FoodModel.js
@@ -3,61 +3,71 @@ import db from "../config/database.js";
 
 // get all Foods
 export const getFoods = (result) => {
-    db.query("SELECT f.*, e.QtaDisponibile  FROM food f LEFT JOIN esauriti e ON f.food_id = e.food_id ORDER BY f.food_category desc, f.food_name asc;", (err,results)=> {
-        if (err){
+    var now = new Date();
+        var day = ("0" + now.getDate()).slice(-2);
+        var month = ("0" + (now.getMonth() + 1)).slice(-2);
+    var datacorrente = now.getFullYear() + month + day
+    console.log(datacorrente)
+    let sql1 = "SELECT f.*, e.QtaDisponibile  FROM food f LEFT JOIN esauriti e ON f.food_id = e.food_id "
+    let sql11 = sql1 + " WHERE f.FlgValidita = 0 "
+    let sql2 = " UNION "
+    let sql3 = sql1 + " WHERE f.FlgValidita != 0 AND " + datacorrente.toString() + " >= f.DataInizioValidita AND " + datacorrente.toString() + " <= f.DataFineValidita ORDER BY food_category desc, food_name asc"
+    let sql = sql11 + sql2 + sql3
+    db.query(sql, (err, results) => {
+        if (err) {
             console.log(err);
-            result(err,null);
-        }else{
-            result(null,results);
+            result(err, null);
+        } else {
+            result(null, results);
         }
     });
 };
 
 // get single Foods
-export const getFoodById = (id,result) => {
-    db.query("SELECT * FROM food WHERE food_id = ?",[id], (err,results)=> {
-        if (err){
+export const getFoodById = (id, result) => {
+    db.query("SELECT * FROM food WHERE food_id = ?", [id], (err, results) => {
+        if (err) {
             console.log(err);
-            result(err,null);
-        }else{
-            result(null,results[0]);
+            result(err, null);
+        } else {
+            result(null, results[0]);
         }
     });
 };
 
 // insert Food
-export const insertFood = (data,result) => {
-    db.query("INSERT INTO food SET ?",data, (err,results)=> {
-        if (err){
+export const insertFood = (data, result) => {
+    db.query("INSERT INTO food SET ?", data, (err, results) => {
+        if (err) {
             console.log(err);
-            result(err,null);
-        }else{
-            result(null,results[0]);
+            result(err, null);
+        } else {
+            result(null, results[0]);
         }
     });
 };
 
 // update Food
-export const updateFoodById = (data,id,result) => {
-    db.query("UPDATE food SET food_name = ?, food_price = ? WHERE food_id = ?",[data.food_name, data.food_price, id], (err,results)=> {
-        if (err){
+export const updateFoodById = (data, id, result) => {
+    db.query("UPDATE food SET food_name = ?, food_price = ? WHERE food_id = ?", [data.food_name, data.food_price, id], (err, results) => {
+        if (err) {
             console.log(err);
-            result(err,null);
-        }else{
-            result(null,results);
+            result(err, null);
+        } else {
+            result(null, results);
         }
     });
 };
 
 
 // delete Food
-export const deleteFoodById = (id,result) => {
-    db.query("DELETE FROM food WHERE food_id = ?",[id], (err,results)=> {
-        if (err){
+export const deleteFoodById = (id, result) => {
+    db.query("DELETE FROM food WHERE food_id = ?", [id], (err, results) => {
+        if (err) {
             console.log(err);
-            result(err,null);
-        }else{
-            result(null,results);
+            result(err, null);
+        } else {
+            result(null, results);
         }
     });
 };
