bf4513e242c3eee0f76ae4de758a1fe301346e53
lpizzi <lpizzi00@gmail.com>
2024-02-12 07:27:15 +0100

    passaggio a https

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 window.axios = axios
 axios.defaults.withCredentials = false
 // axios.defaults.baseURL = "http://localhost:8081/api"
-let URLProd = "http://" + window.location.hostname.toString() + "/api"
+let URLProd = "https://" + window.location.hostname.toString() + "/api"
 let URLDev ="http://" + window.location.hostname.toString() + ":8081/api"
 
 let backendUrl = process.env.NODE_ENV === 'production' ?  URLProd : URLDev 
