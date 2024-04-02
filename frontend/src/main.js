630c025507a9e509a12e33808ec1a48856cf2028
DESKTOP-ISD7DOL\Giuli <giulio.pizzinato@gmail.com>
2024-04-02 21:29:05 +0200

    no message

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,7 +5,7 @@ import router from './router'
 import store from './store'
 import "@/axios"
 
-const currentVersion = '1.2.0'; // Versione corrente impostata durante il deploy
+const currentVersion = '1.2.2'; // Versione corrente impostata durante il deploy
 const eventBus = mitt();
 
 //controllo di versione 
