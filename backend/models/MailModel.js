34cbe5edce84685421f935dc98f95b7d1b995000
DESKTOP-ISD7DOL\Giuli <giulio.pizzinato@gmail.com>
2025-07-28 13:20:28 +0200

    no message

--- backend/models/MailModel.js
+++ backend/models/MailModel.js
@@ -28,28 +28,28 @@ export const Emailsender = (data, result) => {
             <p style="text-align:center; font-family: 'Satisfy', cursive; font-size:25px; color:#27ae60; padding-bottom: 2px;">Articoli Ordinati</p>
             <p style="text-align:center; font-size:16px;">Riportiamo gli articoli che hai ordinato:</p>
             <br>
-            <table  width="100%" border="solid" align="center" margin-bottom="20px"><tr style="background-color: #ffffff;"><td style="background-color: #ffffff; padding-left:10px; padding-top:10px; padding-bottom:10px; font-size:16px;">Articoli</td><td style="background-color: #ffffff; padding-left:10px; padding-top:10px; padding-bottom:10px; font-size:16px; text-align: center;">Quantità` + data.ord_item.join('') + `</td></tr></table><br>
+            <table width="100%" border="solid" align="center" margin-bottom="20px"><tr style="background-color: #ffffff;"><td style="background-color: #ffffff; padding-left:10px; padding-top:10px; padding-bottom:10px; font-size:16px;">Articoli</td><td style="background-color: #ffffff; padding-left:10px; padding-top:10px; padding-bottom:10px; font-size:16px; text-align: center;">Quantità` + data.ord_item.join('') + `</td></tr></table><br>
             <p style="text-align: left; font-size:16px;">Questa email inviata da <a href="` + data.sagra_link + `">ordini.esagra.it</a> per l'ordine inserito alle ore ` + data.ord_data + `<br><br>La ringraziamo per aver utilizzato il nostro servizio online.</p>
         </div>`;
 
         mailOptions.subject = "Il tuo codice dell' Ordine web"
     } else {
         emailText = `<div style=" whidth: 90%; padding-left:4px; padding-right:4px; background-color: #f5f5f5; padding-top: 5px; padding-botton: 5px; border-radius: 10px">
-            <p style="text-align:center; font-family: 'Satisfy', cursive; font-size:25px; color:#27ae60;">Grazie</p>
+            <p style="text-align:center; font-family: 'Satisfy', cursive; font-size:25px; color:#f38609;">Grazie</p>
             <p style="text-align: center; font-size:16px;">Buongiorno gentile cliente.<br>Il tuo codice prenotazione è:</p>
-            <h2 style="text-align:center; font-size:60px; padding-top: 5px; padding-botton: 5px; color:#27ae60; whidth:100%; background-color: #fff;">N.`  + data.ord_id + `</h2>
-            <hr style="border: 2px dashed #27ae60;">
+            <h2 style="text-align:center; font-size:60px; padding-top: 5px; padding-botton: 5px; color:#f38609; whidth:100%; background-color: #fff;">P.`  + data.ord_id + `</h2>
+            <hr style="border: 2px dashed #f38609;">
         </div>
-        <div style=" whidth: 90%; padding-left:4px; padding-right:4px; background-color: #fff; padding-top: 8px; padding-botton: 8px; border-left: 2px dashed #27ae60; border-right:  2px dashed #27ae60;">
-                <p style="text-align:center; font-family: 'Satisfy', cursive; font-size:25px; color:#27ae60; padding-bottom: 2px;">Hai dei ripensamenti?</p>
+        <div style=" whidth: 90%; padding-left:4px; padding-right:4px; background-color: #fff; padding-top: 8px; padding-botton: 8px; border-left: 2px dashed #f38609; border-right:  2px dashed #f38609;">
+                <p style="text-align:center; font-family: 'Satisfy', cursive; font-size:25px; color:#f38609; padding-bottom: 2px;">Hai dei ripensamenti?</p>
                 <p style="text-align: center; font-size:16px;">Se ti sei accorto che nel tuo ordine manca qualcosa,<br>puoi sempre modificarlo cliccando sul bottone qui sotto </p>
                 <p style="margin-top:5px;">ㅤ</p>
-                <a href="` + data.user_data + `" style="padding: 10px 5px; item-aling: center; font-size: 16px; color: #fff; background: #27ae60; border-radius: 10px;">Clicca e modifica l'ordine</a>
+                <a href="` + data.user_data + `" style="padding: 10px 5px; item-aling: center; font-size: 16px; color: #fff; background: #f38609; border-radius: 10px;">Clicca e modifica l'ordine</a>
                 <p style="margin-top:5px;">ㅤ</p>
         </div>
         <div style=" whidth: 90%; padding-left:4px; padding-right:4px; background-color: #f5f5f5; padding-botton: 8px; border-radius: 10px">
-            <hr style="border: 2px dashed #27ae60;">
-            <p style="text-align:center; font-family: 'Satisfy', cursive; font-size:25px; color:#27ae60; padding-bottom: 2px;">Articolo Prenotato</p>
+            <hr style="border: 2px dashed #f38609;">
+            <p style="text-align:center; font-family: 'Satisfy', cursive; font-size:25px; color:#f38609; padding-bottom: 2px;">Articolo Prenotato</p>
             <p style="text-align:center; font-size:16px;">Riportiamo l'articolo che hai prenotato:</p>
             <br>
             <table  width="100%" border="solid" align="center" margin-bottom="20px"><tr style="background-color: #ffffff;"><td style="background-color: #ffffff; padding-left:10px; padding-top:10px; padding-bottom:10px; font-size:16px;">Articoli</td><td style="background-color: #ffffff; padding-left:10px; padding-top:10px; padding-bottom:10px; font-size:16px; text-align: center;">Quantità` + data.ord_item.join('') + `</td></tr></table><br>
